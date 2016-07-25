aeff6259feb78956e2b383eac4aa296bf48a4d37
Jonathan Pike <code@staite.net>
2016-07-25 12:46:55 +0000

    initial

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+error_reporting(0);
+
+require 'vendor/autoload.php';
+
+use MaxMind\Db\Reader;
+
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+$databaseFile = 'geolite2-country.mmdb';
+
+$reader = new Reader($databaseFile);
+$geo = $reader->get($ipAddress);
+$reader->close();
+$country = null;
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+if (isset($geo['country']['iso_code']))
+{
+	$country = array(
+		'code' => $geo['country']['iso_code'],
+		'name' => $geo['country']['names']['en'],
+	);
+}
+
+echo json_encode(array(
+	'country' => $country,
+));
